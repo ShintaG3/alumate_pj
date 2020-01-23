aa21df0a1996c498da345b9ebcc86db298f5e656
ShintaG3 <snishino2013@gmail.com>
2020-01-23 10:00:27 +0000

    contact

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -5,4 +5,5 @@ app_name="index"
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="landingpage/index.html"), name='index'),
+    path('contact/', TemplateView.as_view(template_name="landingpage/contact.html"), name='contact'),
 ]
