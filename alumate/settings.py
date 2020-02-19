0358e77d474566fbf10b5100ac1a1e717fdce6da
ShintaG3 <snishino2013@gmail.com>
2020-02-19 12:43:15 +0000

    people

--- alumate/settings.py
+++ alumate/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'account',
     'chat',
     'inquiry',
+    'people',
 ]
 
 MIDDLEWARE = [
--- alumate/urls.py
+++ alumate/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('account/', include('account.urls', namespace='account')),
     path('chat/', include('chat.urls', namespace='chat')),
     path('inquiry/', include('inquiry.urls', namespace='inquiry')),
+    path('people/', include('people.urls', namespace='people')),
 ]
