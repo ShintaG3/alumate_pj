1f035a3ee4fef6911740b0dcbc8f66f40a72498f
vikas <vikassrinitb@gmail.com>
2020-02-23 14:39:01 +0530

    fix the urls issues

--- alumate/urls.py
+++ alumate/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('account/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('demo/', include('demo.urls', namespace='demo')),
     path('', include('landingpage.urls', namespace='index')),
     path('auths/', include('auths.urls', namespace='auths')),
