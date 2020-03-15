8ce35fc798e55e1c2b6eba39fc696b19cb47c88c
hkoketsu <hiroki.koketsu@gmail.com>
2020-03-15 14:54:28 -0700

    Add contact form and email sending function

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import ContactView
 
 app_name="index"
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="landingpage/index.html"), name='index'),
-    path('contact/', TemplateView.as_view(template_name="landingpage/contact.html"), name='contact'),
+    path('contact/', ContactView.as_view(), name='contact'),
 ]
