2c8d787535812ba5d5bd07944f3c15240d959a38
hkoketsu <hiroki.koketsu@gmail.com>
2020-03-16 08:25:12 -0700

    (Re-reqest) Add contact form

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.views import View
+from .forms import ContactForm
 
 # Create your views here.
+class ContactView(View):
+    template_name = 'landingpage/contact.html'
+    class_form = ContactForm
+    sent = False
+
+    def get(self, request):
+        form = self.class_form()
+        return render(request, self.template_name, {'form': form, 'sent': self.sent})
+
+    def post(self, request):
+        form = self.class_form(request.POST)
+        if form.is_valid():
+            inputs = form.cleaned_data
+            subject = 'Message from {} {} via contact form'\
+                .format(inputs['first_name'], inputs['last_name'])
+            email = inputs['email']
+            message = inputs['message']
+            to = 'alumate.info@gmail.com'
+            send_mail(subject, message, email, [to])
+            self.sent = True
+        return render(request, self.template_name, {'form': form, 'sent': self.sent})
