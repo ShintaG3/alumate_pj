e737feafd4dd9c7c43b23a2966bee11c63f6e53f
ShintaG3 <snishino2013@gmail.com>
2020-03-16 14:33:32 +0000

    Revert "Add contact form and email sending function"

--- landingpage/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea(attrs={"rows": 6}))
