dce99e74a8e86041d3cac234875bc0c1c32abcc7
hkoketsu <hiroki.koketsu@gmail.com>
2020-03-17 10:57:23 -0700

    Add Dokcer for dev

--- app/alumate/settings.py
+++ app/alumate/settings.py
@@ -8,13 +8,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!f7!huaaiut+_wo+n_zl3jpq&$qp5kv!x^2c&af%8d7&c5k*w)'
+SECRET_KEY = os.environ.get("SECRET_KEY", "international")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=1))
 
-ALLOWED_HOSTS = []
-
+# 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
+# For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(" ")
 
 # Application definition
 
