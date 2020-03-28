b5fc6df9b6f81ac0355db03763636cdda5b34ef1
Hiroki Koketsu <hiroki.koketsu@gmail.com>
2020-03-28 10:49:18 -0700

    Add Post view

--- app/feed/urls.py
+++ app/feed/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from .views import PostView
 
 app_name="feed"
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="feed/feed.html"), name='feed'),
-
+    path('', PostView.as_view(), name='feed'),
 ]
--- app/feed/views.py
+++ app/feed/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views import View
 from .models import Post
-# Create your views here.
+from .forms import FeedPostForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
+class PostView(View):
+    form_class = FeedPostForm
+    template_name = 'feed/feed.html'
 
-    
+    def get(self, request, *args, **kwargs):
+        object_list = Post.objects.all()
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form, 'posts': object_list})
 
-
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+        return HttpResponseRedirect(self.request.path_info) # redirect to the same page
