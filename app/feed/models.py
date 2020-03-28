eb856dd6451dca1d87dd578dd10ca81e5f95f56a
Hiroki Koketsu <hiroki.koketsu@gmail.com>
2020-03-28 10:47:38 -0700

    Update post model

--- app/feed/models.py
+++ app/feed/models.py
@@ -1,21 +1,30 @@
+from django import forms
 from django.db import models
 from django.contrib.auth.models import User
 from accounts.models import School, Country, UserProfile, City
 from django.db.models.signals import pre_save
 from alumate.utils import unique_slug_generator, unique_slug_generator_post
+from django.urls import reverse
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.CharField(max_length=5000)
-    post_created_at = models.DateTimeField(auto_now_add=True)
-    post_edited_at = models.DateTimeField(blank=True, null=True)
-    post_likes = models.IntegerField(default=0)
-    post_views = models.IntegerField(default=0)
-    post_replies = models.IntegerField(default=0)
-    slug = models.SlugField(max_length=40, null=True, blank=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(blank=True, null=True)
+    replies = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    shares = models.IntegerField(default=0)
 
+    class Meta:
+        ordering = ('-created_at',)
+    
     def __str__(self):
-        return self.body
+        return self.user + "'s post: " + self.body[:10] + "..."
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)])
+
 
 class Query(models.Model):
     query_creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -41,6 +50,5 @@ def slug_generator_post(sender, instance, *args, **kwargs):
         instance.slug = unique_slug_generator_post(instance)
 
 pre_save.connect(slug_generator, sender=Query)
-pre_save.connect(slug_generator_post, sender=Post)
     
 
