6acf7ed406004c3be3f32979655007f501a7df21
Hiroki Koketsu <hiroki.koketsu@gmail.com>
2020-04-02 11:19:40 -0700

    Fix radio button style

--- app/accounts/forms.py
+++ app/accounts/forms.py
@@ -4,7 +4,7 @@ from .models import BasicInfo, CurrentStatus, About
 
 class BasicInfoForm(forms.ModelForm):
     current_year = date.today().year
-    status = forms.CharField(widget=forms.RadioSelect(choices=CurrentStatus.choices))
+    status = forms.CharField(widget=forms.RadioSelect(attrs={'class': 'custom-control-input'}, choices=CurrentStatus.choices))
     school_start_year = forms.ChoiceField(choices=[("*", "Still planning")]  + [(x, x) for x in range(1940, current_year+4)])
     school_end_year = forms.ChoiceField(choices=[("*", "Still planning")] + [(x, x) for x in range(1940, current_year+10)])
     
