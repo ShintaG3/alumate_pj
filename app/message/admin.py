cfd2d2552a1b9e149b11d8115b9bcda4aa353ca1
Hiroki Koketsu <36804811+hkoketsu@users.noreply.github.com>
2020-04-16 13:33:02 -0700

    Messages (#41)
    * Remove dropdown attr from message in nav bar
    
    * Enable hover styling for a card in message list
    
    * Enable messaging
    
    * Add detail page
    
    * Remove image upload for reply
    
    * Enable direct message from account page
    
    * Hide change profile image button for not owner
    
    * Use smaller avator for message detail
    
    * Fix end if tag inside div

--- app/message/admin.py
+++ app/message/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import message
+from .models import Message
 # Register your models here.
 
-admin.site.register(message)
+admin.site.register(Message)
 
 
