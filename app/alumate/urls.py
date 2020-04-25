678cd58847a552fb542b7c452c6bd899ed42b041
vikas <vikassrinitb@gmail.com>
2020-04-25 10:31:12 +0530

    SMTPAuthenticationError

--- app/alumate/urls.py
+++ app/alumate/urls.py
@@ -20,12 +20,16 @@ urlpatterns = [
 ]
 
 urlpatterns +=[
-    path('password-reset/', 
-        auth_views.PasswordResetView.as_view(template_name='auths/password_reset_form.html'), 
-        name='password_reset'),
-    path('password-reset/done/', 
-        auth_views.PasswordResetDoneView.as_view(template_name='auths/password_reset_done.html'), 
-        name='password_reset_done'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='auths/password_reset_form.html'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='auths/password_reset_done.html'
+         ),
+         name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='auths/password_reset_confirm.html'
          ),
