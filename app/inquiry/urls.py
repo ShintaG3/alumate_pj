99078a2b2fe7ea0d2be86434834af697100641a1
Hiroki Koketsu <hiroki.koketsu@gmail.com>
2020-04-24 19:25:59 -0700

    Search of inquiry

--- app/inquiry/urls.py
+++ app/inquiry/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('like/<int:id>', InquiryLikeView.as_view(), name="like"),
     path('comment/like/<int:ask_id>/<int:comment_id>', InquiryCommentLikeView.as_view(), name="like-comment"),
     path('comment/<int:id>', InquiryCommentView.as_view(), name="comment"),
-    path('follow/<int:id>/<str:username>', FollowView.as_view(), name="follow")
+    path('follow/<int:id>/<str:username>', FollowView.as_view(), name="follow"),
+    path('update-result', update_result, name="update-result"),
 ]
--- app/inquiry/views.py
+++ app/inquiry/views.py
@@ -3,10 +3,11 @@ from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import get_object_or_404, redirect, reverse
+from django.shortcuts import get_object_or_404, redirect, reverse, render
 from .models import *
 from .forms import *
 from accounts.models import Follow
+import json
 
 
 # Create your views here.
@@ -14,8 +15,18 @@ class InquiryView(LoginRequiredMixin, TemplateView):
     template_name = 'inquiry/inquiry.html'
 
     def get_context_data(self, **kwargs):
-        user = self.request.user        
+        user = self.request.user
+        
+        home_country_options = set(AskTagHomeCountry.objects.all().values_list('body', flat=True))
+        study_abroad_country_options = set(AskTagStudyAbroadCountry.objects.all().values_list('body', flat=True))
+        country_options = home_country_options.union(study_abroad_country_options)
+        school_options = set(AskTagSchool.objects.all().values_list('body', flat=True))
+        major_options = set(AskTagMajor.objects.all().values_list('body', flat=True))
+
         context = {
+            'country_options': country_options,
+            'school_options': school_options,
+            'major_options': major_options,
             'ask_list': self.get_ask_list(user)
         }
         return context
@@ -25,7 +36,6 @@ class InquiryView(LoginRequiredMixin, TemplateView):
         asks = Ask.objects.all()[:10]
         for ask in asks:
             school_tags = list(AskTagSchool.objects.filter(ask=ask).values_list('body', flat=True))
-            print(school_tags)
             school_tags_str = ', '.join(school_tags)
 
             try:
@@ -37,9 +47,73 @@ class InquiryView(LoginRequiredMixin, TemplateView):
                 'value': ask,
                 'liked': liked,
                 'school_tag': school_tags_str,
+                'searched': False
             })
 
         return ask_list
+
+
+def update_result(request, *args, **kwargs):
+    query_dict = json.loads(request.body)
+    user = request.user
+
+    search_result = get_search_result(query_dict, user)
+    ask_list = []
+
+    for ask in search_result:
+        school_tags = list(AskTagSchool.objects.filter(ask=ask).values_list('body', flat=True))
+        school_tags_str = ', '.join(school_tags)
+
+        try:
+            liked = AskLike.objects.get(user=user, ask=ask)
+        except AskLike.DoesNotExist:
+            liked = None
+
+        ask_list.append({
+            'value': ask,
+            'liked': liked,
+            'school_tag': school_tags_str,
+        })
+    
+    context = {
+        'ask_list': ask_list,
+        'searched': True,
+        'results': search_result.count()
+    }
+
+    return render(request, 'inquiry/result.html', context=context)
+
+def get_search_result(query_params, user):
+    search_result = Ask.objects.all()
+    for key in query_params.keys():
+        value_list = query_params.get(key)
+        if not value_list:
+            continue
+        
+        ask_id_list = None
+        if key == 'status':
+            ask_id_list = AskTagStatus.objects.filter(body__in=value_list).values_list('ask__id', flat=True)
+        elif key == 'home_countries':
+            ask_id_list = AskTagHomeCountry.objects.filter(body__in=value_list).values_list('ask__id', flat=True)
+        elif key == 'study_abroad_countries':
+            ask_id_list = AskTagStudyAbroadCountry.objects.filter(body__in=value_list).values_list('ask__id', flat=True)
+        elif key == 'schools':
+            ask_id_list = AskTagSchool.objects.filter(body__in=value_list).values_list('ask__id', flat=True)
+        elif key == 'majors':
+            ask_id_list = AskTagMajor.objects.filter(body__in=value_list).values_list('ask__id', flat=True)
+        elif key == 'start_year':
+            lower_bound = int(value_list[0][0:4])
+            upper_bound = int(value_list[1][0:4])
+            ask_id_list = AskTagStartYear.objects.filter(lower_bound__gte=lower_bound, upper_bound__lte=upper_bound).values_list('ask__id', flat=True)
+        elif key == 'end_year':
+            lower_bound = int(value_list[0][0:4])
+            upper_bound = int(value_list[1][0:4])
+            ask_id_list = AskTagEndYear.objects.filter(lower_bound__gte=lower_bound, upper_bound__lte=upper_bound).values_list('ask__id', flat=True)
+
+        if ask_id_list:
+            search_result = search_result.filter(id__in=ask_id_list)
+
+    return search_result
 
 class InquiryDetailView(LoginRequiredMixin, TemplateView):
     template_name = 'inquiry/inquiry-detail.html'
