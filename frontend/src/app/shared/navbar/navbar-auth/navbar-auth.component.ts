78e5ea170f0a88f1161bedea88df26976ab7432e
Hiroki Koketsu <hiroki.koketsu@gmail.com>
2020-05-04 14:10:31 -0700

    Nav bar

--- frontend/src/app/shared/navbar/navbar-auth/navbar-auth.component.ts
+++ frontend/src/app/shared/navbar/navbar-auth/navbar-auth.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar-auth.component.css']
 })
 export class NavbarAuthComponent implements OnInit {
+  isCollapsed = true;
 
   constructor() { }
 
