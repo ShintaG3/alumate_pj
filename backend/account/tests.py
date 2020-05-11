29ff2ae9a0b9dc33f082c85e3660aba7883cc461
Hiroki Koketsu <hiroki.koketsu@gmail.com>
2020-05-11 12:10:22 -0700

    Account: school test cases

--- backend/account/tests.py
+++ backend/account/tests.py
@@ -5,6 +5,7 @@ from . import models, views
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+
 def get_auth_client():
     user_data = {"username": "testuser", "password": "fortesting"}
     client = APIClient()
@@ -14,6 +15,7 @@ def get_auth_client():
                        jwt_response.data['access'])
     return client
 
+
 class CountryTestCase(TestCase):
 
     def setUp(self):
@@ -21,7 +23,7 @@ class CountryTestCase(TestCase):
         self.url = reverse('account:countries')
         if models.Country.objects.count() != 0:
             models.Country.objects.clear()
-    
+
     def test_api_get_all_countries(self):
         self.assertEqual(models.Country.objects.count(), 0)
         models.Country.objects.create(name='Japan')
@@ -32,17 +34,49 @@ class CountryTestCase(TestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(models.Country.objects.count(), 2)
-    
+
     def test_api_get_filtered_countries(self):
         self.assertEqual(models.Country.objects.count(), 0)
         models.Country.objects.create(name='Japan')
         models.Country.objects.create(name='USA')
-        response = self.client.get('{}?starts-with=j'.format(self.url) )
+        response = self.client.get('{}?starts-with=j'.format(self.url))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
-            
 
     def test_api_cannot_create_country(self):
         response = self.client.post(self.url)
-        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-    
+        self.assertEqual(response.status_code,
+                         status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class SchoolTestCase(TestCase):
+
+    def setUp(self):
+        self.client = get_auth_client()
+        self.url = reverse('account:schools')
+        if models.School.objects.count() != 0:
+            models.School.objects.clear()
+
+    def test_api_get_all_schools(self):
+        self.assertEqual(models.School.objects.count(), 0)
+        models.School.objects.create(name='UBC')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.School.objects.count(), 1)
+        models.School.objects.create(name='UT')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.School.objects.count(), 2)
+
+    def test_api_get_filtered_schools(self):
+        self.assertEqual(models.School.objects.count(), 0)
+        models.School.objects.create(name='UBC')
+        models.School.objects.create(name='UT')
+        response = self.client.get('{}?starts-with=ub'.format(self.url))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_api_cannot_create_school(self):
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code,
+                         status.HTTP_405_METHOD_NOT_ALLOWED)
